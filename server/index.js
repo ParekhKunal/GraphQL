1e8a09a114d4ed3e0a83d6a0782ad865863997d4
KunalParekh <82876469+ParekhKunal@users.noreply.github.com>
2024-10-02 11:30:51 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,68 @@
+require('dotenv').config()
+
+const express = require('express')
+const { ApolloServer } = require('@apollo/server')
+const { expressMiddleware } = require('@apollo/server/express4')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const axios = require('axios')
+
+const { USERS } = require('./users')
+const { TODOS } = require('./todos')
+
+async function init() {
+
+    const app = express()
+    const server = new ApolloServer({
+        typeDefs: `
+            type User{
+                id: ID!
+                name: String!
+                username: String!
+                email: String!
+                phone: String!
+                website: String!
+            }
+
+            type Todo{
+                id: ID!
+                title: String!
+                completed: Boolean
+                user: User
+            }
+            
+            type Query {
+                getTodos: [Todo]
+                getAllUsers: [User]
+                getUser(id: ID!): User
+            }
+        `,
+        resolvers: {
+            Todo: {
+                user: (todo) => USERS.find((e) => e.id === todo.id),
+            },
+            Query: {
+                getTodos: () => TODOS,
+                getAllUsers: () => USERS,
+                getUser: async (parent, { id }) => USERS.find((e) => e.id === id),
+            }
+        }
+    })
+
+    app.use(bodyParser.json())
+    app.use(cors())
+
+    await server.start()
+    app.use('/graphql', expressMiddleware(server))
+
+    app.use('/', (req, res) => {
+        res.send('Hello World')
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on ${process.env.PORT} + http://localhost:8001`)
+    })
+
+}
+
+init()
